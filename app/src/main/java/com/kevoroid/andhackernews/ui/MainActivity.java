57b11924f63443935e87110171a94b3f9e4bad10
Kevin <kevoroid@gmail.com>
2017-05-27 18:03:35 +0700

    main activity instantiating of list fragment

--- app/src/main/java/com/kevoroid/andhackernews/ui/MainActivity.java
+++ app/src/main/java/com/kevoroid/andhackernews/ui/MainActivity.java
@@ -1,15 +1,45 @@
 package com.kevoroid.andhackernews.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
 import com.kevoroid.andhackernews.R;
 
 public class MainActivity extends BaseActivity {
+
+    private boolean mLastBackPressedState = false;
+    private Fragment mStoryListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mStoryListFragment == null) {
+            mStoryListFragment = StoryListFragment.newInstance();
+            setFragment(mStoryListFragment);
+        } else {
+            setFragment(mStoryListFragment);
+        }
+    }
+
+    protected void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_activity_fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (mStoryListFragment.isVisible()) {
+            finish();
+        }
     }
 }
