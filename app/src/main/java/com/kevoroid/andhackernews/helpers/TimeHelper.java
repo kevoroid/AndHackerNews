f9162c183973a8bce3913181e9d1688fec4dde2b
Kevin <kevoroid@gmail.com>
2017-05-28 21:56:45 +0700

    time helper class update

--- app/src/main/java/com/kevoroid/andhackernews/helpers/TimeHelper.java
+++ app/src/main/java/com/kevoroid/andhackernews/helpers/TimeHelper.java
@@ -1,5 +1,9 @@
 package com.kevoroid.andhackernews.helpers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by kevin on 5/27/17.
  */
@@ -10,11 +14,17 @@ public class TimeHelper {
         return System.currentTimeMillis();
     }
 
-    public static int getUnixTimestamp() {
-        return (int) (getCurrentEpochTime() / 1000L);
+    public static long getUnixEpochTime() {
+        return getCurrentEpochTime() / 1000L;
     }
 
     public static boolean isTimePassed(long startTime, long thresholdMills) {
         return getCurrentEpochTime() - startTime > thresholdMills;
+    }
+
+    public static String returnActualDate(String epoch) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+        long actualDate = Long.parseLong(epoch) * 1000L;
+        return sdf.format(new Date(actualDate));
     }
 }
