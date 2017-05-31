73cccd45a6e1ce9e148c8b0586661991d5cc49ac
Kevin <kevoroid@gmail.com>
2017-05-31 20:50:57 +0700

    cleaning up request maker

--- app/src/main/java/com/kevoroid/andhackernews/api/RequestMaker.java
+++ app/src/main/java/com/kevoroid/andhackernews/api/RequestMaker.java
@@ -5,21 +5,23 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 /**
  * Created by kevin on 5/28/17.
  */
 
-public class RequestMaker<T> extends BaseRequest<T> {
+public class RequestMaker extends BaseRequest {
 
-    private final Response.Listener<T> mListener;
+    private final Response.Listener mListener;
+    private String mResponseBody;
 
     public static final String HN_WEB_URL = "https://news.ycombinator.com";
     public static final String HN_API_URL = "https://hacker-news.firebaseio.com/v0/";
     public static final String HN_TOP_STORIES_URL = HN_API_URL + "topstories.json";
     public static final String HN_ITEM_URL = HN_API_URL + "/item/";
 
-    public RequestMaker(int method, String url, Response.Listener<T> responseListener, Response.ErrorListener errorListener) {
+    public RequestMaker(int method, String url, Response.Listener responseListener, Response.ErrorListener errorListener) {
         super(method, url, responseListener, errorListener);
         mListener = responseListener;
     }
@@ -27,15 +29,17 @@ public class RequestMaker<T> extends BaseRequest<T> {
     @Override
     protected Response parseNetworkResponse(NetworkResponse networkResponse) {
         try {
-            String responseBody = new String(networkResponse.data, HttpHeaderParser.parseCharset(networkResponse.headers));
+            mResponseBody = new String(networkResponse.data, HttpHeaderParser.parseCharset(networkResponse.headers));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return Response.success(networkResponse, HttpHeaderParser.parseCacheHeaders(networkResponse));
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add(mResponseBody);
+        return Response.success(stringArrayList, HttpHeaderParser.parseCacheHeaders(networkResponse));
     }
 
     @Override
-    protected void deliverResponse(T o) {
+    protected void deliverResponse(Object o) {
         mListener.onResponse(o);
     }
 }
