3ac500d775c6d00e0fa459874e59e4dd32dc5b09
Kevin <kevoroid@gmail.com>
2017-06-04 19:36:08 +0700

    adding unit test for sample story object

--- app/src/test/java/com/kevoroid/andhackernews/RequestParseUnitTest.java
+++ app/src/test/java/com/kevoroid/andhackernews/RequestParseUnitTest.java
@@ -1,7 +1,10 @@
 package com.kevoroid.andhackernews;
 
+import com.kevoroid.andhackernews.adapters.StoryListAdapter;
 import com.kevoroid.andhackernews.ui.StoryListFragment;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,5 +19,31 @@ public class RequestParseUnitTest {
         StoryListFragment storyListFragment = new StoryListFragment();
         String tempArrayString = storyListFragment.formatJsonObject(TestResponse.mainStoriesIds.replace(" ", "")).toString();
         Assert.assertEquals(TestResponse.convertToArray(), tempArrayString);
+    }
+
+    @Test
+    public void ResponseElementsTest() throws Exception {
+        JSONObject jsonObject = new JSONObject(TestResponse.mainStoryItem);
+        Assert.assertNotNull(jsonObject);
+
+        StoryListAdapter storyListAdapter = new StoryListAdapter();
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_TITLE, 0));
+        Assert.assertEquals(jsonObject.getString(StoryListAdapter.RESULT_FIELD_TITLE), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_TITLE, 0));
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_BY, 0));
+        Assert.assertEquals(jsonObject.getString(StoryListAdapter.RESULT_FIELD_BY), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_BY, 0));
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_TIME, 0));
+        Assert.assertEquals(String.valueOf(jsonObject.getInt(StoryListAdapter.RESULT_FIELD_TIME)), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_TIME, 0));
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_SCORE, 0));
+        Assert.assertEquals(String.valueOf(jsonObject.getInt(StoryListAdapter.RESULT_FIELD_SCORE)), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_SCORE, 0));
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_URL, 0));
+        Assert.assertEquals(jsonObject.getString(StoryListAdapter.RESULT_FIELD_URL), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "string", StoryListAdapter.RESULT_FIELD_URL, 0));
+
+        Assert.assertNotNull(storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "array", StoryListAdapter.RESULT_FIELD_KIDS, 0));
+        Assert.assertEquals(String.valueOf(jsonObject.getJSONArray(StoryListAdapter.RESULT_FIELD_KIDS)), storyListAdapter.returnObjectValueNamed(new JSONArray().put(jsonObject), "array", StoryListAdapter.RESULT_FIELD_KIDS, 0));
     }
 }
