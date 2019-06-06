bf235daa4b46cae88ee97c96df3816a2681fda0b
kevoroid <kevoroid@gmail.com>
2019-06-06 14:00:53 +0700

    update to application class for leakcanary

--- app/src/main/java/com/kevoroid/andhackernews/AndHackerNewsApplication.java
+++ app/src/main/java/com/kevoroid/andhackernews/AndHackerNewsApplication.java
@@ -4,15 +4,17 @@ import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
 
-/**
- * Created by kevin on 5/28/17.
- */
-
 public class AndHackerNewsApplication extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        LeakCanary.install(this);
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		initLeakCanary();
+	}
+
+	private void initLeakCanary() {
+		if (BuildConfig.DEBUG) {
+			LeakCanary.install(this);
+		}
+	}
 }
