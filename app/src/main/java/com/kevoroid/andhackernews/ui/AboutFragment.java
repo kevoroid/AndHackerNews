cd3538693509ca886c3fea3d1d7713f59ac8e7bd
kevoroid <kevoroid@gmail.com>
2019-06-06 16:59:10 +0700

    implementing About fragment

--- app/src/main/java/com/kevoroid/andhackernews/ui/AboutFragment.java
+++ app/src/main/java/com/kevoroid/andhackernews/ui/AboutFragment.java
@@ -1,4 +1,50 @@
 package com.kevoroid.andhackernews.ui;
 
-public class AboutFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.kevoroid.andhackernews.R;
+
+public class AboutFragment extends BaseFragment {
+
+	private ActionBar actionBar;
+
+	public static AboutFragment newInstance() {
+
+		Bundle args = new Bundle();
+
+		AboutFragment fragment = new AboutFragment();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		if (getActivity() != null) {
+			actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+		}
+		if (actionBar != null) {
+			actionBar.setTitle("About");
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.about_fragment, container, false);
+		return v;
+	}
 }
