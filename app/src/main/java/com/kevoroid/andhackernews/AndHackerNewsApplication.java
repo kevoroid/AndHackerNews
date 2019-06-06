ae4e007515a07d3acdf6d6835cc990b2c5ddd49a
kevoroid <kevoroid@gmail.com>
2019-06-06 14:04:45 +0700

    new leakcanary update, no need to init leakcanary anymore

--- app/src/main/java/com/kevoroid/andhackernews/AndHackerNewsApplication.java
+++ app/src/main/java/com/kevoroid/andhackernews/AndHackerNewsApplication.java
@@ -2,19 +2,20 @@ package com.kevoroid.andhackernews;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class AndHackerNewsApplication extends Application {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		initLeakCanary();
+		//initLeakCanary();
 	}
 
+	/*
 	private void initLeakCanary() {
 		if (BuildConfig.DEBUG) {
 			LeakCanary.install(this);
 		}
 	}
+
+	 */
 }
