0b0def39df9692b640364c3cf7840102368ce036
kevoroid <kevoroid@gmail.com>
2019-11-04 14:44:35 +0700

    adding sonar run script

---
+++ run_sonar.sh
@@ -0,0 +1,1 @@
+./gradlew sonarqube -Dsonar.projectKey=kevoroid_AndHackerNews -Dsonar.organization=kevoroid-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=6dcf3821370f0f96cd7b4e5ff10461bcaba36502
